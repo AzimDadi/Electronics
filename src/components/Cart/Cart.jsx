c110992dd734f883140c5c05f1da4f2e88a78d26
Azim Dadi <dadiazim11@gmail.com>
2023-06-22 12:17:15 +0530

    initial commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,34 @@
+import { useContext } from "react";
 import "./Cart.scss";
 import CartItem from "./CartItem/CartItem";
 
 import { MdClose } from "react-icons/md";
-// import { BsCartX } from "react-icons/bs";
+import { BsCartX } from "react-icons/bs";
+import { Context } from "../../utils/context";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentRequest } from "../../utils/api";
+
 const Cart = ({ setShowCart }) => {
+  const { cartItems, cartSubTotal } = useContext(Context);
+
+  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest.post("/api/orders", {
+        products: cartItems,
+      });
+
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="cart-panel">
       <div className="opec-layer"></div>
@@ -16,23 +41,29 @@ const Cart = ({ setShowCart }) => {
           </span>
         </div>
 
-        {/* <div className="empty-cart">
-          <BsCartX />
-          <span>No products in the cart.</span>
-          <button className="return-cta">RETURN TO SHOP</button>
-        </div> */}
-        <>
-          <CartItem />
-          <div className="cart-footer">
-            <div className="subtotal">
-              <span className="text">Subtotal:</span>
-              <span className="text total">&#8377;999</span>
-            </div>
-            <div className="button">
-              <button className="checkout-cta">Checkout</button>
-            </div>
+        {!cartItems?.length && (
+          <div className="empty-cart">
+            <BsCartX />
+            <span>No products in the cart.</span>
+            <button className="return-cta">RETURN TO SHOP</button>
           </div>
-        </>
+        )}
+        {!!cartItems?.length && (
+          <>
+            <CartItem />
+            <div className="cart-footer">
+              <div className="subtotal">
+                <span className="text">Subtotal:</span>
+                <span className="text total">&#8377;{cartSubTotal}</span>
+              </div>
+              <div className="button">
+                <button className="checkout-cta" onClick={handlePayment}>
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,21 @@ import { TbSearch } from "react-icons/tb";
 import { AiOutlineHeart } from "react-icons/ai";
 import { CgShoppingCart } from "react-icons/cg";
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import "./Header.scss";
 import Cart from "../Cart/Cart";
 import Search from "./Search/Search";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../../utils/context";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const [showCart, setShowCart] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
+  const {cartCount} = useContext(Context)
+
+  const navigate = useNavigate();
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -31,17 +36,19 @@ const Header = () => {
       <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
         <div className="header-content">
           <ul className="left">
-            <li>Home</li>
+            <li onClick={() => navigate("/")}>Home</li>
             <li>About</li>
             <li>Categories</li>
           </ul>
-          <div className="center">MOBILEWORLD</div>
+          <div className="center" onClick={() => navigate("/")}>
+            MOBILEWORLD
+          </div>
           <div className="right">
             <TbSearch onClick={() => setShowSearch(true)} />
             <AiOutlineHeart />
             <span className="cart-icon" onClick={() => setShowCart(true)}>
               <CgShoppingCart />
-              <span>5</span>
+              {!!cartCount &&<span>{cartCount}</span>}
             </span>
           </div>
         </div>
